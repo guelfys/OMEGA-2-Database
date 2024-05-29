207ad4478a20646539a0d6da1567db2ebf47d744
cristiandjr <cristiandjr89@gmail.com>
2024-05-29 15:07:43 -0300

    add: se elimina en funcionalidades: factura manual y factura

--- 06_funcionalidades.sql
+++ 06_funcionalidades.sql
@@ -1,5 +1,4 @@
 INSERT INTO dbo.funcionalidad (nombre, categoria, descripcion, ruta) VALUES
-    ('Factura', NULL, NULL, '/factura'),
     ('Medio pago', NULL, NULL, '/mediopago'),
     ('Empresa', NULL, NULL, '/empresa'),
     ('Tipo de servicio', NULL, NULL, '/tipodeservicio'),
@@ -7,14 +6,13 @@ INSERT INTO dbo.funcionalidad (nombre, categoria, descripcion, ruta) VALUES
     ('Servicio', NULL, NULL, '/servicio'),
     ('Proveedor', NULL, NULL, '/proveedor'),
     ('Sitios', NULL, NULL, '/sitios'),
-    ('Factura manual', NULL, NULL, '/facturamanual'),
     ('Facturas con incumplimientos', NULL, NULL, '/facturasconincumplimientos'),
     ('Regla de negocio', NULL, NULL, '/regladenegocio'),
     ('Selects', NULL, NULL, '/selects'),
     ('Gestión Inmobiliaria', NULL, NULL, '/gestioninmobiliaria'),
     ('Factura OCR', NULL, NULL, '/facturaocr'),
     ('Conciliación de pagos', NULL, NULL, '/conciliaciondepagos'),
-    ('reportes', NULL, NULL, '/reportes'),
+    ('Reportes', NULL, NULL, '/reportes'),
     ('Gestión de servicios', NULL, NULL, '/gestiondeservicios');
 	
 GO
